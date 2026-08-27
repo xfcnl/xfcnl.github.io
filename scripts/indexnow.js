b0c96ac718007c3d0ab45b707e6d2da867f3aa40
xiaofen <G114514g@yeah.net>
2026-08-27 21:29:39 +0800

    为博客增加IndexNow支持

---
+++ scripts/indexnow.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const HOSTS = ["xfcnl.github.io", "husd.cc.cd", "blog.sfvg.de5.net"];
+const ENDPOINT = "https://api.indexnow.org/indexnow";
+
+function readUrls() {
+  const sitemapPath = path.join(process.cwd(), "public", "sitemap.xml");
+  const sitemap = fs.readFileSync(sitemapPath, "utf8");
+  const urls = [];
+  const regex = /<loc>(.*?)<\/loc>/g;
+  let match;
+  while ((match = regex.exec(sitemap)) !== null) {
+    urls.push(match[1].trim());
+  }
+  return urls;
+}
+
+function rewriteHost(url, host) {
+  const u = new URL(url);
+  return u.protocol + "//" + host + u.pathname + u.search + u.hash;
+}
+
+async function submit(host, key, urlList) {
+  const body = JSON.stringify({
+    host,
+    key,
+    keyLocation: "https://" + host + "/" + key,
+    urlList,
+  });
+  const res = await fetch(ENDPOINT, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body,
+  });
+  const text = await res.text();
+  console.log("[" + host + "] " + res.status + " " + res.statusText);
+  if (!res.ok) {
+    console.log(text);
+  }
+  return res.ok;
+}
+
+async function main() {
+  const key = process.env.INDEXNOW_KEY;
+  if (!key) {
+    throw new Error("INDEXNOW_KEY env is not set");
+  }
+  const urls = readUrls();
+  if (!urls.length) {
+    throw new Error("No URLs found in public/sitemap.xml");
+  }
+  console.log("Submitting " + urls.length + " URLs for " + HOSTS.length + " hosts");
+
+  let allOk = true;
+  for (const host of HOSTS) {
+    const urlList = urls.map((u) => rewriteHost(u, host));
+    allOk = (await submit(host, key, urlList)) && allOk;
+  }
+  if (!allOk) {
+    process.exitCode = 1;
+  }
+}
+
+if (require.main === module) {
+  main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
+}
---
+++ themes/omagari-hare/scripts/indexnow-key.js
@@ -0,0 +1,12 @@
+hexo.extend.generator.register("indexnow-key", function () {
+  const key = process.env.INDEXNOW_KEY;
+  if (!key) {
+    hexo.log.warn("INDEXNOW_KEY not set, skip generating IndexNow key file");
+    return;
+  }
+  return {
+    path: key,
+    data: key,
+    layout: false,
+  };
+});
